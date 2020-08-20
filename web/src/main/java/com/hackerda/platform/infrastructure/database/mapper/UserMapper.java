5822f29888fb985393843694d074ebe4b2d9de40
junrong.chen <546159053@qq.com>
2020-08-20 10:51:04 +0800

    内容发布

---
+++ web/src/main/java/com/hackerda/platform/infrastructure/database/mapper/UserMapper.java
@@ -0,0 +1,35 @@
+package com.hackerda.platform.infrastructure.database.mapper;
+
+import com.hackerda.platform.infrastructure.database.model.User;
+import com.hackerda.platform.infrastructure.database.model.UserExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
