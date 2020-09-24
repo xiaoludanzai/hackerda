ac93d5e9f12f6ee229ee7fee58fd4e204eb7a69d
junrong.chen <546159053@qq.com>
2020-09-24 09:59:32 +0800

    时间戳

--- web/src/main/java/com/hackerda/platform/controller/vo/AppMessageVO.java
+++ web/src/main/java/com/hackerda/platform/controller/vo/AppMessageVO.java
@@ -23,7 +23,7 @@ public class AppMessageVO {
 
     private long postId;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern="yyyy/MM/dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
 }
