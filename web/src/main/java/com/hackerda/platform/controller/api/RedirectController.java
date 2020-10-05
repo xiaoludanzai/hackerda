29e512bdca878926fcba88ce5059515d6e6017c6
junrong.chen <546159053@qq.com>
2020-10-05 15:20:47 +0800

    重定向域名

--- web/src/main/java/com/hackerda/platform/controller/api/RedirectController.java
+++ web/src/main/java/com/hackerda/platform/controller/api/RedirectController.java
@@ -8,12 +8,12 @@ import javax.servlet.http.HttpServletResponse;
 @Controller("/redirect")
 public class RedirectController {
 
-    @RequestMapping("axinfu")
+    @RequestMapping("/axinfu")
     public void axinfu(HttpServletResponse resp) throws Exception {
         resp.sendRedirect("https://h5.axinfu.com/login/inputMobile");
     }
 
-    @RequestMapping("repair")
+    @RequestMapping("/repair")
     public void repair(HttpServletResponse resp) throws Exception {
         resp.sendRedirect("https://koudaigou.net/web/formview/59e0c16ebb7c7c67aea6713d");
     }
